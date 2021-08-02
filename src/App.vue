5df75f8aaf45f9c30f7f770048defa34adbaadec
Tusharbalar <balar.tushar1@gmail.com>
2021-08-02 12:17:24 +0530

    Set mobile screen message

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,46 @@
 <template>
-  <router-view />
+  <div>
+    <div v-if="!mobile" class="app flex flex-column">
+      <Navigation />
+      <div class="app-content flex flex-column">
+        <router-view />
+      </div>
+    </div>
+    <div v-else class="mobile-message flex flex-column">
+      <h2>Sorry, this app is not supported on Mobile Devices</h2>
+      <p>To use this app, please use a computer or Tablet</p>
+    </div>
+  </div>
 </template>
+
+<script>
+import Navigation from './components/Navigation.vue';
+export default {
+  data() {
+    return {
+      mobile: null
+    }
+  },
+  components: {
+    Navigation
+  },
+  created() {
+    this.checkScreen();
+    window.addEventListener("resize", this.checkScreen);
+  },
+  methods: {
+    checkScreen() {
+      const windowWidth = window.innerWidth;
+      if (windowWidth <= 750) {
+        this.mobile = true;
+        return;
+      }
+      this.mobile = false;
+    }
+  }
+};
+</script>
+
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
@@ -9,8 +49,33 @@
   padding: 0;
   box-sizing: border-box;
   font-family: "Poppins", sans-serif;
-  background-color: #141625;
 }
+.app {
+  background-color: #141625;
+  min-height: 100vh;
+  @media (min-width: 900px) {
+    flex-direction: row !important;
+  }
+
+  .app-content {
+    padding: 0 20px;
+    flex: 1;
+    position: relative;
+  }
+}
+
+.mobile-message {
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #141625;
+  color: #fff;
+  p {
+    margin-top: 16px;
+  }
+}
+
 button,
 .button {
   cursor: pointer;
